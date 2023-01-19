8b933f19415ba47d3c92329ef0a0f4a08649b886
Rob Moore <rob.moore@makerx.com.au>
2023-01-19 23:50:11 +0800

    Updates to the instructions and experience

--- deploy/asset.ts
+++ deploy/asset.ts
@@ -1,33 +1,38 @@
-import { ethers, hardhatArguments, run } from "hardhat";
-import { getIMXAddress, getEnv, sleep } from "./utils";
+import { ethers, hardhatArguments } from "hardhat";
+import { getIMXAddress, getEnv } from "./utils";
 
 async function main() {
-    const [deployer] = await ethers.getSigners();
+  const [deployer] = await ethers.getSigners();
 
-    console.log("Deploying Contract with the account:", deployer.address);
-    console.log("Account Balance:", (await deployer.getBalance()).toString());
+  console.log("Deploying Contract with the account:", deployer.address);
+  console.log("Account Balance:", (await deployer.getBalance()).toString());
 
-    if (!hardhatArguments.network) {
-        throw new Error("please pass --network");
-    }
+  if (!hardhatArguments.network) {
+    throw new Error("please pass --network");
+  }
 
-    const owner = getEnv("OWNER_ACCOUNT_ADDRESS");
-    const name = getEnv("CONTRACT_NAME");
-    const symbol = getEnv("CONTRACT_SYMBOL");
-    const baseUri = getEnv("CONTRACT_METADATA_URI");
+  const owner = getEnv("OWNER_ACCOUNT_ADDRESS");
+  const name = getEnv("CONTRACT_NAME");
+  const symbol = getEnv("CONTRACT_SYMBOL");
+  const baseUri = getEnv("CONTRACT_METADATA_URI");
 
-    const Asset = await ethers.getContractFactory("Asset");
-    const imxAddress = getIMXAddress(hardhatArguments.network);
-    const asset = await Asset.deploy(owner, name, symbol, baseUri, imxAddress);
-    console.log("\n\nDeployed successfully!!");
-    console.log("View on etherscan: https://goerli.etherscan.io/address/" + asset.address);
-    console.log("\nUpdate the following in your .env file");
-    console.log(`CONTRACT_ADDRESS=\"${asset.address}\"\n`);
+  const Asset = await ethers.getContractFactory("Asset");
+  const imxAddress = getIMXAddress(hardhatArguments.network);
+  const asset = await Asset.deploy(owner, name, symbol, baseUri, imxAddress);
+  console.log();
+  console.log();
+  console.log("Deployed successfully!!");
+  console.log(
+    "View on etherscan: https://goerli.etherscan.io/address/" + asset.address
+  );
+  console.log();
+  console.log("Update the following in your .env file:");
+  console.log(`CONTRACT_ADDRESS=${asset.address}`);
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- scripts/generateRandomKey.ts
+++ scripts/generateRandomKey.ts
@@ -1,10 +1,12 @@
 import Wallet from 'ethereumjs-wallet';
 
+console.log('Generating a new Ethereum wallet with a random private key...')
+
 const wallet = Wallet.generate();
 
 console.log(`
-  Update the following values in the .env file
+Update the following values in the .env file:
 
-  OWNER_ACCOUNT_ADDRESS="${wallet.getAddressString()}"
-  OWNER_ACCOUNT_PRIVATE_KEY="${wallet.getPrivateKeyString()}"
+OWNER_ACCOUNT_ADDRESS=${wallet.getAddressString()}
+OWNER_ACCOUNT_PRIVATE_KEY=${wallet.getPrivateKeyString()}
 `);

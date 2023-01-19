e623c46f912f97b9b8d147f10cbcabb071a2a377
Richard Watts-Seale <rwattsseale@gmail.com>
2023-01-19 22:40:07 +1100

    Update to latest solidity compiler

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,7 +11,7 @@ dotenv.config();
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.17",
   networks: {
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
